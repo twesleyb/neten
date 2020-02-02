a0c2233ab92073ea9f51d2bb4ba20249e2d79c21
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-01 20:11:58 -0500

    adding data, fingers crossed

---
+++ R/butterfly-data.R
@@ -0,0 +1,17 @@
+#' Butterfly data
+#'
+#' @docType data
+#'
+#' @usage data("butterfly")
+#'
+#' @format An object of class \code{"matrix"}.
+#'
+#' @keywords datasets
+#'
+#' @references Wang et al., 2018
+#'
+#' @source \href{}{}
+#'
+#' @examples
+#' data("butterfly")
+"butterfly"
---
+++ R/butterflyNE-data.R
@@ -0,0 +1,17 @@
+#' Butterfly data after Network Enhancment with orignal Matlab implementation
+#'
+#' @docType data
+#'
+#' @usage data("butterflyNE")
+#'
+#' @format An object of class \code{"matrix"}.
+#'
+#' @keywords datasets
+#'
+#' @references Wang et al., 2018
+#'
+#' @source \href{}{}
+#'
+#' @examples
+#' data("butterflyNE")
+"butterflyNE"
