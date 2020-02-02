99a38a94296f6dbf64fdfce90d408368d479aa51
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-01 20:23:42 -0500

    fixed K->k

--- R/Network_Enhancement.R
+++ R/Network_Enhancement.R
@@ -38,7 +38,7 @@ Network_Enhancement <- function(W_in,
     W_in <- as.matrix(W_in)
   }
   eps <- 2e-16
-  force(K)
+  force(k)
   W_in1 <- W_in * (1 - diag(Length(W_in)))
   zeroindex <- which(colSums(abs(W_in)) > 0)
   W0 <- W_in[zeroindex, zeroindex]
@@ -48,14 +48,14 @@ Network_Enhancement <- function(W_in,
   if (length(unique(as.vector(W))) == 2) {
     P <- W
   } else {
-    P <- dominateset(abs(W), min(K, Length(W) - 1)) * sign(W)
+    P <- dominateset(abs(W), min(k, Length(W) - 1)) * sign(W)
   }
   P <- P + (diag(Length(P)) + diag(rowSums(abs(P))))
   P <- TransitionFields(P)
   eig <- eigen(P)
   U <- eig$vectors
   d <- eig$values - eps
-  d <- (1 - alpha) * d / (1 - alpha * d^order)
+  d <- (1 - alpha) * d / (1 - alpha * d^diffusion)
   D <- diag(d)
   W <- U %*% D %*% t(U)
   W <- W * (1 - diag(Length(W))) / (1 - diag(W))
